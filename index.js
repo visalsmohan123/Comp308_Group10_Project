1ed5f0bf9a01eb392f0537e055c9bab659139643
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-04-18 17:43:02 -0400

    modified connection timeout

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.use(cors()); // Enable CORS
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase the connection timeout
+    socketTimeoutMS: 30000, // Increase the socket timeout
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Could not connect to MongoDB:', err));
